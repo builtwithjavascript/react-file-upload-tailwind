5834f200bbe6c1cac73290ba00c321fbc5f68983
damianof <damianof@users.noreply.github.com>
2024-04-23 08:07:18 -0400

    export also types from file input validator

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,11 @@
 // file: src/components/index.ts
+export type { 
+  IFileInfo, 
+  IFileValidatorItem, 
+  IFileValidatorOptions,
+  IFileValidator,
+  FileInputValidatorItems
+} from '@builtwithjavascript/file-input-validator'
 export * from './FileInput.component'
 export * from './FileValidator.component'
 export * from './FileValidatorRow.component'

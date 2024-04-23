ea77c3c6e245f3852f0257c79ed98a9b138e4f54
damianof <damianof@users.noreply.github.com>
2024-04-23 08:08:23 -0400

    pretty

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,11 @@
 // file: src/components/index.ts
-export type { 
-  IFileInfo, 
-  IFileValidatorItem, 
+export type {
+  IFileInfo,
+  IFileValidatorItem,
   IFileValidatorOptions,
   IFileValidator
 } from '@builtwithjavascript/file-input-validator'
-export {
-  FileInputValidatorItems
-} from '@builtwithjavascript/file-input-validator'
+export { FileInputValidatorItems } from '@builtwithjavascript/file-input-validator'
 export * from './FileInput.component'
 export * from './FileValidator.component'
 export * from './FileValidatorRow.component'

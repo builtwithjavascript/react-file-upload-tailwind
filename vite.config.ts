d20a7c15bd44125660a961ca20d0e36f9788d0f0
damianof <damianof@users.noreply.github.com>
2026-08-04 15:37:05 -0400

    v1.2.0 npm deps + ts and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { name } from './package.json'
-const projectName = name.replace('@builtwithjavascript/', '').trim().toLowerCase()
+import pkg from './package.json' with { type: 'json' }
+
+const projectName = pkg.name.replace('@builtwithjavascript/', '').trim().toLowerCase()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +16,7 @@ export default defineConfig({
   envDir: './src/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src/')
+      '@': path.resolve(import.meta.dirname, 'src/')
     },
   },
   test: {
@@ -27,7 +28,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: path.resolve(import.meta.dirname, 'src/index.ts'),
       name: projectName,
       fileName: (format) => `${projectName}.${format}.js`,
     },
